678c14e5e699b4611592fbc32a50783c1075ae30
Kennedy1127 <zxc1258741@gmail.com>
2023-05-15 20:38:57 +0800

    update

--- src/js/controller/membership/membershipController.js
+++ src/js/controller/membership/membershipController.js
@@ -1,6 +1,10 @@
 import navigationView from "./../../views/navigationView.js";
+import membershipNavigationView from "../../views/membership/membershipNavigationView.js";
+import membershipOverlayView from "../../views/membership/membershipOverlayView.js";
 
 const init = () => {
   navigationView.init();
+  membershipNavigationView.init();
+  membershipOverlayView.init();
 };
 init();
---
+++ src/js/views/membership/membershipOverlayView.js
@@ -0,0 +1,45 @@
+class MembershipOverlayView {
+  _commentButtons = document.querySelectorAll(
+    ".membership_booking_info_comment_button"
+  );
+  _body = document.querySelector("body");
+  _overlay = document.querySelector(".membership_overlay");
+  _cancelButton = document.querySelector(".membership_overlay_card_cancel");
+
+  _checkOverlay() {
+    this._overlay.classList.contains("membership_overlay--active")
+      ? this._body.classList.add("no-scroll")
+      : this._body.classList.remove("no-scroll");
+  }
+
+  _toggleOverlay() {
+    this._overlay.classList.toggle("membership_overlay--active");
+    this._body.classList.toggle("no-scroll");
+  }
+
+  _showOverlay() {
+    this._commentButtons.forEach((button) =>
+      button.addEventListener("click", () => this._toggleOverlay())
+    );
+  }
+
+  _hiddenOverlay() {
+    this._cancelButton.addEventListener("click", () => {
+      this._toggleOverlay();
+    });
+
+    this._overlay.addEventListener("click", (e) => {
+      if (e.target.classList.contains("membership_overlay--active")) {
+        this._toggleOverlay();
+      }
+    });
+  }
+
+  init() {
+    this._checkOverlay();
+    this._showOverlay();
+    this._hiddenOverlay();
+  }
+}
+
+export default new MembershipOverlayView();
