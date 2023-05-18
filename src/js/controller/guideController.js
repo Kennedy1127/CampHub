d591aacc3ef97d8c48ceea83503907835369e85a
Kennedy Chin <zxc1258741@gmail.com>
2023-05-18 23:48:28 +0800

    update

--- src/js/controller/guideController.js
+++ src/js/controller/guideController.js
@@ -1,8 +1,10 @@
 import navigationView from "../views/navigationView.js";
+import guideView from "../views/guide/guideView.js";
 import stepView from "../views/guide/stepView.js";
 
 const init = () => {
   navigationView.init();
+  guideView.init();
   stepView.setupStepHeaders();
 };
 init();
---
+++ src/js/views/guide/guideView.js
@@ -0,0 +1,20 @@
+class GuideView {
+  init() {
+    const guides = document.querySelectorAll(".guide_step");
+    const steps = document.querySelectorAll(".step");
+    console.log(guides);
+    console.log(steps);
+
+    guides.forEach((guide, i) =>
+      guide.addEventListener("click", (e) => {
+        const guide = e.target.closest(".guide_step");
+        if (!guide) return;
+
+        const indexOfStep = [...guides].indexOf(guide);
+        steps[indexOfStep].scrollIntoView({ behavior: "smooth" });
+      })
+    );
+  }
+}
+
+export default new GuideView();
