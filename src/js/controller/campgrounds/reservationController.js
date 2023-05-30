2ace627a417cae40edc0233932a85f85c509d124
Kennedy1127 <zxc1258741@gmail.com>
2023-05-30 16:50:55 +0800

    update

--- src/js/controller/campgrounds/reservationController.js
+++ src/js/controller/campgrounds/reservationController.js
@@ -1,8 +1,6 @@
 import navigationView from "../../views/navigationView.js";
-import calendarView from "../../views/reservation/calendarView.js";
 
 const init = () => {
-  navigationView.clickMenuButton();
-  calendarView.init();
+  navigationView.init();
 };
 init();
