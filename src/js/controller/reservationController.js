c45a01af494bca2d9bfd7d61fd970855f030ea56
Kennedy1127 <zxc1258741@gmail.com>
2023-05-30 10:35:07 +0800

    test

--- src/js/controller/reservationController.js
+++
@@ -1,8 +0,0 @@
-import navigationView from "../views/navigationView.js";
-import calendarView from "../views/reservation/calendarView.js";
-
-const init = () => {
-  navigationView.clickMenuButton();
-  calendarView.init();
-};
-init();
